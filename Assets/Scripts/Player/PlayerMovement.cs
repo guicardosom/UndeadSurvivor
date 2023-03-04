439d02590e55d7a3726684ea73753ad0b1bd287c
gccmiranda <gccardosomiranda@outlook.com>
2023-03-03 23:03:00 -0500

    Flip sprites according to direction of movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,35 +4,60 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] GameObject rightHand;
     [SerializeField] private float moveSpeed = 5f;
+
     private Rigidbody2D body;
+    private SpriteRenderer sprite;
+
     private Vector2 movement;
+
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        getPlayerInput();
+        GetPlayerInput();
+        FlipSprite();
     }
 
     //moving player in FixedUpdate because it handles physics better
     void FixedUpdate()
     {
-        movePlayer();
+        MovePlayer();
     }
 
-    void getPlayerInput()
+    void GetPlayerInput()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
     }
 
-    void movePlayer()
+    void MovePlayer()
     {
         //normalize movement so we don't have the issue where moveSpeed is doubled when moving diagonally
         body.MovePosition(body.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void FlipSprite()
+    {
+        //flip hand and player's sprites according to direction of movement
+        SpriteRenderer handSprite = rightHand.GetComponent<SpriteRenderer>();
+
+        if (movement.x == -1)
+        {
+            sprite.flipX = true;
+            handSprite.flipX = true;
+        }
+
+        if (movement.x == 1)
+        {
+            sprite.flipX = false;
+            handSprite.flipX = false;
+        }
     }
 }
