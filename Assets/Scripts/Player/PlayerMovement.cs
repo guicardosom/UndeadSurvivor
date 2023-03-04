ed64f3d56701dc69f8671463caca581c06651ced
gccmiranda <gccardosomiranda@outlook.com>
2023-03-03 23:06:52 -0500

    Rotate gun towards cursor checkpoint

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     private SpriteRenderer sprite;
 
     private Vector2 movement;
+    private Vector2 mousePos;
 
 
     void Start()
@@ -22,6 +23,8 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         GetPlayerInput();
+        GetMousePosition();
+        RotateTowardsCursor();
         FlipSprite();
     }
 
@@ -41,6 +44,19 @@ public class PlayerMovement : MonoBehaviour
     {
         //normalize movement so we don't have the issue where moveSpeed is doubled when moving diagonally
         body.MovePosition(body.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void GetMousePosition()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void RotateTowardsCursor()
+    {
+        //rotate hand to follow mouse position
+        Vector2 lookDirection = mousePos - body.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        rightHand.transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 
     void FlipSprite()
